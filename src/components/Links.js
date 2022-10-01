47e094006872894992688016c8e6b9602ee46786
Elalisami <ali.sami@ukh.edu.krd>
2022-10-01 17:23:37 +0300

    Done

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,8 +4,8 @@ function Links(props) {
   return (
     <div id="links">
     <h3>Links</h3>
-    <a href={props.github}>github</a>
-    <a href={props.linkedin}>Linkedin</a>
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin}>{props.linkedin}</a>
   </div>
   )
 }
